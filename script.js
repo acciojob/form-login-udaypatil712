d795b04103cc84cb8fc385fabfb2e5189802a9e4
Uday Patil <patiluday7122003@gmail.com>
2025-11-20 15:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+	let Firstname = document.querySelector("input[name="fname"]").value.trim();
+	let Lastname = document.querySelector("input[name="lname"]").value.trim();
+
+	let fullname = Firstname + " " + Lastname;
+	
+	alert(fullname);
 
 }
